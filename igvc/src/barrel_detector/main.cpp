f0945158177c82702460f125f3b52ac1eaa0635a
Tejasvi Nareddy <tejunareddy@gmail.com>
2016-04-13 20:00:11 -0400

    Removed unnecesary code.

--- igvc/src/barrel_detector/main.cpp
+++ igvc/src/barrel_detector/main.cpp
@@ -1,44 +1,38 @@
 #include <cmath>
 #include <pcl_ros/point_cloud.h>
-#include <sensor_msgs/LaserScan.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
 
 using namespace pcl;
 using namespace ros;
 using namespace std;
-using namespace tf;
 
 Publisher _pointcloud_pub;
+tf::TransformListener *tf_listener;
 set<string> frames_seen;
 
 void frame_callback(const PointCloud<PointXYZ>::ConstPtr &msg, const string &topic) {
     PointCloud<PointXYZ> transformed;
     tf::StampedTransform transform;
-    if(frames_seen.find(msg->header.frame_id) == frames_seen.end()) {
-        frames_seen.insert(msg->header.frame_id);
-        tf_listener->waitForTransform("/map", msg->header.frame_id, Time(0), Duration(5));
-    }
 
-    tf_listener->lookupTransform("/map", msg->header.frame_id, Time(0), transform);
-    pcl_ros::transformPointCloud(*msg, transformed, transform);
-    for (auto point : transformed) {
-        point.z = 0;
-    }
+    // TODO
 
-    frames[topic] = transformed;
-
-    publish_map();
-
-    _pointcloud_pub.publish(*cloud_for_pub);
+    _pointcloud_pub.publish(transformed);
 }
 
 int main(int argc, char** argv) {
-    init(argc, argv, "scan_to_pointcloud");
+    // FIXME
+    init(argc, argv, "barreldetector");
 
     NodeHandle nh;
 
-    _pointcloud_pub = nh.advertise<PointCloud<PointXYZ> >("/barrel/blackout_image", 1);
+    PotholeDetector det{nh};
+
+    tf_listener = new tf::TransformListener();
+    _pointcloud_pub = nh.advertise<PointCloud<PointXYZ>>("/barrel/blackout_image", 1);
 
     Subscriber scan_sub = nh.subscribe("/scan/pointcloud", 1, frame_callback);
-
     spin();
+
+    return 0;
 }
